67de760dfe88c874e22b19c52bb3dfa7292bbfbd
Beatrizvzz-lab <lirabeatriz.001@gmail.com>
2025-06-22 16:14:54 -0300

    feat: Add swagger

---
+++ src/swagger.ts
@@ -0,0 +1,26 @@
+import type { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Omni Wallet API')
+    .setDescription('API RESTful para simulação de transações monetárias')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+}
